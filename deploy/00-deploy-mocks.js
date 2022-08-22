3bb916c22571387aa5c8058e275303a7fae0b901
sandalwoods <1764076874@qq.com>
2022-08-22 19:10:57 +0800

    dynamic

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,5 +1,5 @@
-const { getNamedAccounts, deployments, network, ethers } = require("hardhat")
-const { developmentChains } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+const { developmentChains, DECIMALS, INITIAL_PRICE } = require("../helper-hardhat-config")
 
 const BASE_FEE = ethers.utils.parseEther("0.25") //0.25 is the premium, it costs 0.25 LINK per request
 const GAS_PRICE_LINK = 1e9 //link per gas //calculated value based on the gas price of the chain
@@ -8,19 +8,25 @@ module.exports = async function ({ getNamedAccounts, deployments}) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
-    const args = [BASE_FEE, GAS_PRICE_LINK]
 
     if(developmentChains.includes(network.name)) {
         log("local network detected! deploying mocks...")
         //deploy a mock vrfcoordinator...
         await deploy("VRFCoordinatorV2Mock", {
             from: deployer,
-            args: args,
+            args: [BASE_FEE, GAS_PRICE_LINK],
             log: true,
         })
+
+        await deploy("MockV3Aggregator", {
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_PRICE],
+        })
+
         log("Mocks deployed!")
         log("----------------------------------------------")
     }
 }
 
-module.exports.tags = ["all", "mocks"]
+module.exports.tags = ["all", "mocks", "main"]
---
+++ deploy/03-deploy-dynamic-svg-nft.js
@@ -0,0 +1,38 @@
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+const fs = require("fs")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    const chainId = network.config.chainId
+    let ethUsdPriceFeedAddress
+
+    if (developmentChains.includes(network.name)) {
+        const EthUsdAggregator = await ethers.getContract("MockV3Aggregator")
+        ethUsdPriceFeedAddress = EthUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed
+    }
+
+    log("--------deploying-----------------")
+    const lowSVG = fs.readFileSync("./images/dynamicNft/frown.svg", {encoding: "utf-8"})
+    const highSVG = fs.readFileSync("./images/dynamicNft/happy.svg", {encoding: "utf-8"})
+    args = [ethUsdPriceFeedAddress, lowSVG, highSVG]
+    const dynamicSvgNft = await deploy("DynamicSvgNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("verifying...")
+        await verify(dynamicSvgNft.address, args)
+    }
+    log("--------deployed-----------------")
+}
+
+module.exports.tags = ["all", "dynamicsvg", "main"]
